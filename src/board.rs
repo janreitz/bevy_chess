4c057789de6187ece599eb612f825323553e0149
Jan Reitz <jan-reitz@gmx.de>
2021-01-02 22:18:09 +0100

    Give each sqare an individual material to be able to color them separately

--- src/board.rs
+++ src/board.rs
@@ -20,9 +20,6 @@ pub fn create_board(
     // Add meshes and materiales
     // meshes.add() returns a Handle<Mesh>
     let mesh = meshes.add(Mesh::from(shape::Plane {size:1.0}));
-    let white_material = materials.add(Color::rgb(1.0,0.9,0.9).into());
-    let black_material = materials.add(Color::rgb(0.0,0.1,0.1).into());
-
     // Spawn 64 squares
     for i in 0..8{
         for j in 0..8 {
@@ -30,9 +27,9 @@ pub fn create_board(
                 mesh: mesh.clone(),
                 // Alternate material
                 material: if (i + j + 1) % 2 == 0 {
-                    white_material.clone()
+                    materials.add(Color::rgb(1.0,0.9,0.9).into())
                 } else {
-                    black_material.clone()
+                    materials.add(Color::rgb(0.0,0.1,0.1).into())
                 },
                 transform: Transform::from_translation( Vec3::new(i as f32, 0.0, j as f32)),
                 ..Default::default()
