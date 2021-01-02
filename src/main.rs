b19a9d8ce782f4ca9aff2ee9a57d1b28574e9a16
Jan Reitz <jan-reitz@gmx.de>
2021-01-02 21:09:41 +0100

    Customize Window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build().add_plugins(DefaultPlugins).run();
+    App::build()
+        .add_resource(Msaa { samples: 4})
+        .add_resource(WindowDescriptor {
+            title: String::from("Chess!"),
+            width: 1600.0,
+            height: 1600.0,
+            // This is struct update syntax, filling out the remainder 
+            // of the struct with default values as provided by this struct
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
