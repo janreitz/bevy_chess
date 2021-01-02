2d7242956e21e0a437d454b57e43f4feed1376c6
Jan Reitz <jan-reitz@gmx.de>
2021-01-02 21:30:39 +0100

    Seperate board creation from setup and implement chess board pattern

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,8 @@ use bevy::prelude::*;
 
 fn setup(
     commands: &mut Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands
-        // Plane
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Plane {size: 8.0})),
-            material: materials.add(Color::rgb(1.0, 0.9, 0.9).into()),
-            transform: Transform::from_translation(Vec3::new(4.0,0.0,4.0)),
-            ..Default::default()
-        })
         // Camera
         .spawn(Camera3dBundle {
             transform: Transform::from_matrix(Mat4::from_rotation_translation(Quat::from_xyzw(-0.3, -0.5,-0.3,0.5).normalize(), Vec3::new(-7.0, 20.0, 4.0),
@@ -24,6 +15,35 @@ fn setup(
             transform: Transform::from_translation(Vec3::new(4.0, 8.0,4.0)),
             ..Default::default()
         });
+}
+
+fn create_board(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Add meshes and materiales
+    // meshes.add() returns a Handle<Mesh>
+    let mesh = meshes.add(Mesh::from(shape::Plane {size:1.0}));
+    let white_material = materials.add(Color::rgb(1.0,0.9,0.9).into());
+    let black_material = materials.add(Color::rgb(0.0,0.1,0.1).into());
+
+    // Spawn 64 squares
+    for i in 0..8{
+        for j in 0..8 {
+            commands.spawn(PbrBundle {
+                mesh: mesh.clone(),
+                // Alternate material
+                material: if (i + j + 1) % 2 == 0 {
+                    white_material.clone()
+                } else {
+                    black_material.clone()
+                },
+                transform: Transform::from_translation( Vec3::new(i as f32, 0.0, j as f32)),
+                ..Default::default()
+            });
+        }
+    }
 }
 
 fn main() {
@@ -42,6 +62,9 @@ fn main() {
         .add_startup_system(
             // calling `system()` on a function turns it into a system
             setup.system()
+        )
+        .add_startup_system(
+            create_board.system()
         )
         .run();
 }
