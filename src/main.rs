47ca0bdf0661e8b76464bbf3d5be61a6e638dd25
Jan Reitz <jan-reitz@gmx.de>
2021-01-02 21:21:01 +0100

    Add a startup system setting up a plane, camera and lighting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use bevy::prelude::*;
 
+fn setup(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        // Plane
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane {size: 8.0})),
+            material: materials.add(Color::rgb(1.0, 0.9, 0.9).into()),
+            transform: Transform::from_translation(Vec3::new(4.0,0.0,4.0)),
+            ..Default::default()
+        })
+        // Camera
+        .spawn(Camera3dBundle {
+            transform: Transform::from_matrix(Mat4::from_rotation_translation(Quat::from_xyzw(-0.3, -0.5,-0.3,0.5).normalize(), Vec3::new(-7.0, 20.0, 4.0),
+        )),
+        ..Default::default()
+        })
+        // Light
+        .spawn(LightBundle {
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0,4.0)),
+            ..Default::default()
+        });
+}
+
 fn main() {
     App::build()
         .add_resource(Msaa { samples: 4})
@@ -12,5 +38,10 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        // Startup systems are called only one, at startup
+        .add_startup_system(
+            // calling `system()` on a function turns it into a system
+            setup.system()
+        )
         .run();
 }
